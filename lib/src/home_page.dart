0777a8b1e7fcfcf278158f2a9dc930f00f075c1e
Chunlee Thong <chunlee.thong@gmail.com>
2021-07-18 02:19:31 +0700

    update status bar

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -10,6 +11,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nimet Sanha"),
+        centerTitle: true,
+        backwardsCompatibility: false,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.light,
+          statusBarBrightness: Brightness.dark,
+          statusBarColor: Colors.blue,
+        ),
+      ),
+      body: Center(
+        child: Text(
+          "Home",
+          style: Theme.of(context).textTheme.headline5,
+        ),
+      ),
+    );
   }
 }
--- lib/src/splash_screen_page.dart
+++ lib/src/splash_screen_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nimet_sanha/src/home_page.dart';
 
 class SplashScreenPage extends StatefulWidget {
   SplashScreenPage({Key? key}) : super(key: key);
@@ -8,8 +9,27 @@ class SplashScreenPage extends StatefulWidget {
 }
 
 class _SplashScreenPageState extends State<SplashScreenPage> {
+  Future onSplashing() async {
+    await Future.delayed(Duration(seconds: 3));
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+  }
+
+  @override
+  void initState() {
+    onSplashing();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          "assets/angkor-wat.png",
+          width: 144,
+          // height: 200,
+        ),
+      ),
+    );
   }
 }
